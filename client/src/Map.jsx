6c6f3bf8b7e5bf54903d2916ab158c8a9f5928c8
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-05-28 19:20:51 +0200

    Add icons + own location to map

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,57 +1,111 @@
 import React, { useEffect, useLayoutEffect, useState, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import './assets/scss/index.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+// icons
+import iconMarker from './assets/icons/png/marker-stroked.png';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia29lbmhhYWdzbWEiLCJhIjoiY2wzbjNuY255MGF3ODNwbnl2amJuYms4MCJ9.QD5jhV_KLgBjGYcGOFnwTg';
 
 export default function Map() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(5.433048);
-    const [lat, setLat] = useState(52.295109);
+    const mapContainerRef = useRef(null);
+    const [mapLoaded, setMapLoaded] = useState(null);
+
     const [zoom, setZoom] = useState(6);
     const [markers, setMarkers] = useState();
-    const [marker, setMarker] = useState();
+    const [currentLocation, setCurrentLocation] = useState();
 
+    // Get current position
     useEffect(() => {
         (async () => {
-            const url = `http://localhost:4242/sr?lng=${lng}&lat=${lat}`;
+            const setPosition = (position) => {
+                setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
+            };
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(setPosition);
+            } else {
+                return false;
+            }
+        })();
+    }, []);
+
+    // Fetch loadingpoles close to current position
+    useEffect(() => {
+        if (!currentLocation) return;
+        (async () => {
+            const url = `http://localhost:4242/sr?lng=${currentLocation.lng}&lat=${currentLocation.lat}`;
             console.log(url);
             const response = await fetch(url);
             const data = await response.json();
             setMarkers(data);
         })();
-    }, []);
+    }, [currentLocation]);
 
-    useEffect(() => {
-        console.log(markers);
-    }, [markers]);
-
+    // Mount map
     useLayoutEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
+        if (mapLoaded || !currentLocation) return;
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
+            center: [currentLocation.lng, currentLocation.lat],
             zoom: zoom,
         });
-    });
+        setMapLoaded(map);
 
-    useLayoutEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
+        map.on('load', () => {
+            map.loadImage(iconMarker, (error, image) => {
+                if (error) throw error;
+                map.addImage('iconMarker', image);
+
+                map.addSource('points', {
+                    type: 'geojson',
+                    data: {
+                        type: 'FeatureCollection',
+                        features: [
+                            {
+                                type: 'Feature',
+                                properties: {
+                                    title: 'Center (You)',
+                                    'marker-symbol': 'monument',
+                                },
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [currentLocation.lng, currentLocation.lat],
+                                },
+                            },
+                        ],
+                    },
+                });
+
+                // Add a symbol layer
+                map.addLayer({
+                    id: 'points',
+                    type: 'symbol',
+                    source: 'points',
+                    layout: {
+                        'icon-image': 'iconMarker',
+                        'text-field': ['get', 'title'],
+                        'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                        'text-offset': [0, 1.25],
+                        'text-anchor': 'top',
+                    },
+                });
+            });
         });
-    });
+
+        // Remove map on unmount
+        return () => map.remove();
+    }, [currentLocation]);
 
     return (
         <div>
             <div className="sidebar">
-                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                {currentLocation &&
+                    `Current location: latitude: ${currentLocation.lat} | longitude: ${currentLocation.lng}`}
             </div>
-            <div ref={mapContainer} className="map-container" />
+            <div ref={mapContainerRef} className="map-container" />
         </div>
     );
 }
