0ea8e6bbdc614395d5670fc5dc278c74981230d9
Koen Haagsma <koen.haagsma@themobilecompany.com>
2022-06-10 13:13:25 +0200

    Delete all server old

--- server_old/helpers/formatLngLat.js
+++
@@ -1,9 +0,0 @@
-const formatLngLat = (query, decimals, offset, plus) => {
-    if (!plus) {
-        return (parseFloat(query) - offset).toFixed(decimals);
-    } else {
-        return (parseFloat(query) + offset).toFixed(decimals);
-    }
-};
-
-module.exports = { formatLngLat };
