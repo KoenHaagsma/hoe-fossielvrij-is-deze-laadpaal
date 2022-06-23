c5b040b60459deaa94d286626a2c909535ed1c9d
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-06-23 09:28:32 +0200

    Edit content line

--- public/js/modules/renderElement.js
+++ public/js/modules/renderElement.js
@@ -1,10 +1,8 @@
 function renderElementAndClean(parent, html, position) {
     while (parent.firstChild) {
         parent.firstChild.remove();
-        console.log('cleaned');
     }
     parent.insertAdjacentHTML(position, html);
-    console.log('cleaned All');
 }
 
 function renderElement(parent, html, position) {
